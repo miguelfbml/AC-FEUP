ae7951aa63cb2ac27e8ba3a303235c0f04273037
Miguel Lima <up202108659@up.pt>
2024-12-05 23:12:17 +0000

    11 pred

--- proj/script.py
+++ proj/script.py
@@ -1,18 +1,39 @@
 import pandas as pd
 
 # Replace 'your_file.csv' with the path to your CSV file
-csv_file_path = 'basketballPlayoffs/teams.csv'
+csv_file_path = 'winners/playoffs_10.csv'
+
+csv11 = 'year11/teams.csv'
 
 # Read the CSV file
-df = pd.read_csv(csv_file_path)
+playoffs10 = pd.read_csv(csv_file_path)
 
-# Describe the DataFrame
-description = df.describe()
+playoffs11 = pd.read_csv(csv11)
 
-# Print the description
-print(description)
 
-# Get the number of unique teams
-unique_teams = df['tmID'].nunique()
+# Get the unique tmID values from both DataFrames
+teams10 = set(playoffs10['tmID'].unique())
+teams11 = set(playoffs11['tmID'].unique())
 
-print(unique_teams)
+
+listaDeles = ["IND","NYL","ATL","WAS","PHO","LAS","SEA","SAS"]
+
+bestP = ["ATL","CON","IND","LAS","PHO","SAS","SEA","WAS"]
+
+listaDeles = set(listaDeles)
+
+# Find the intersection of tmID values in both sets
+common_teams = teams10.intersection(bestP)
+
+
+
+# Print the common teams
+print("Teams that appear in both playoffs10 and playoffs11:")
+print(list(common_teams))
+
+
+
+
+
+
+
